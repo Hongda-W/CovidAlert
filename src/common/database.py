249add68ef836b190173788582af5ebc9386329d
Hongda-W <hongda.wang@colorado.edu>
2020-06-20 10:25:41 -0600

    User MongoLab for Heroku

--- src/common/database.py
+++ src/common/database.py
@@ -1,10 +1,9 @@
-# import os
+import os
 import pymongo
 
 
 class Database(object):
-    URI = "mongodb://127.0.0.1:27017/covid"
-    # URI = os.environ.get("MONGOLAB_URI")
+    URI = os.environ.get("MONGOLAB_URI")
     DATABASE = None
 
     @staticmethod
